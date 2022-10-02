827d00ca38c4b920aedfb022b4b9eb0d24f0eb16
umerazaz <62714295+umerazaz@users.noreply.github.com>
2022-10-02 08:33:31 -0700

    Delete nest_typeorm directory

--- nest_typeorm/src/config/typeorm.config-migrations.ts
+++
@@ -1,3 +0,0 @@
-import { DataSource, DataSourceOptions } from 'typeorm';
-import { typeOrmConfig } from './typeorm.config';
-export const AppDataSource = new DataSource(typeOrmConfig as DataSourceOptions);
